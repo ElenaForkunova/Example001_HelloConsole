acd91e25af327ae3cbbc24419e797252d3414b1d
Elena Forkunova <forkelena@yandex.ru>
2022-10-17 23:47:28 +0400

    Домашка2задачаМинМах

---
+++ Home001_MinMax/Program.cs
@@ -0,0 +1,15 @@
+﻿int a = 2;
+int b = 10;
+
+int max = a;
+int min = a;
+
+if(a > max) max = a;
+if(b > max) max = b;
+Console.WriteLine("max = ");
+Console.WriteLine(max);
+
+if(min > a) min = a;
+if(min > b) min = b;
+Console.WriteLine("min = ");
+Console.WriteLine(min);
