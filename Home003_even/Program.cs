bcbe010b04238b7c8bcf25f86a3cdd61ec50f919
Elena Forkunova <forkelena@yandex.ru>
2022-10-18 00:26:21 +0400

    Домашка6ЗадачаЧетное

---
+++ Home003_even/Program.cs
@@ -0,0 +1,11 @@
+﻿int a;
+            Console.WriteLine("введите число: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            if (a % 2 == 0)
+            {
+                Console.WriteLine("четное");
+            }
+            else
+            {
+                Console.WriteLine("нечетное");
+            }    
