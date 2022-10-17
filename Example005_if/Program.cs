599815a1a3a2b66d97fe828d4bb59530fd7aa31d
Elena Forkunova <forkelena@yandex.ru>
2022-10-17 22:16:56 +0400

    Example005_ifelse

---
+++ Example005_if/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите миня пользователя ");
+string username = Console.ReadLine();
+
+if (username.ToLower() == "леночек")
+{ 
+    Console.WriteLine("Ура, это Леночек!");
+}
+else
+{
+    Console.Write ("Привет, ");
+    Console.WriteLine(username);    
+}
