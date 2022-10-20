288a3c6c6b2302a04d40e71a1b391bac24574696
Elena Forkunova <forkelena@yandex.ru>
2022-10-20 20:14:29 +0400

    требуется найти первый элемент массива, равный find

---
+++ Example010_find/Program.cs
@@ -0,0 +1,18 @@
+﻿//требуется найти элемент массива, раный find
+int [] array = {1,2,4,8,5,6,4,8};
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+while (index < n)
+{
+    if(array [index] == find)
+    {
+        Console.WriteLine(index);
+        //прерви - break
+        break;
+    }
+    // index = index + 1;
+    index++;
+}
