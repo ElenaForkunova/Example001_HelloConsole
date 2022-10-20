03c52ffccc47e51a6fe75eeeaf1fcbcf96190a00
Elena Forkunova <forkelena@yandex.ru>
2022-10-20 23:30:46 +0400

    ищем расстояние между 2 точками коодинат что-то на непонятнтом

---
+++ proekt003_3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите координаты первой точки");
+int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите координаты 2 точки");
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+double Distance(int x1, int y1,int x2,int y2)
+{
+    double result = 0;
+    result = Math.Sqrt(Math.Pow(x2-x1,2) + Math.Pow(y2-y1,2));
+    return result;
+}
+double result = Distance(x1, y1, x2, y2);
+Console.WriteLine(result);
