1fcbf73aeb4cdfa221022f7671cbcc6a902e9310
Elena Forkunova <forkelena@yandex.ru>
2022-10-23 12:39:00 +0400

    Найти максимальное из трех чисел

---
+++ proekt002_001Max/Program.cs
@@ -0,0 +1,24 @@
+﻿// Найти максимальное из трех чисел
+Console.Write("введи число а ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введи число b ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("введи число c ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+//переменная, которая будет хранить итог
+int max = a;
+if (b > max)
+{
+    max =b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+
+Console.WriteLine($"Максимальное из трех чисел {max}");
+Console.ReadKey();
